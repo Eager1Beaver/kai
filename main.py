993dc5c5e4be82c0812efd7cb4eed9d29f260f54
Eager1Beaver <i1got1power@gmail.com>
2025-10-24 20:11:37 +0300

    main entry

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-# main.py — Entry point for KAI GUI project
+from src.ui.app import main
 
-from src.main_controller import launch_app
-
-if __name__ == '__main__':
-    print('Program started')
-    print('Launching KAI Calcium Signal GUI...')
-    launch_app()
-    print('Program finished')
+if __name__ == "__main__":
+    main()
